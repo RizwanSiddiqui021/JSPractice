3e511bbbab5154f36429800c08b46e1197d466b4
RizwanSiddiqui021 <rizwansiddiqui021@gmail.com>
2025-11-26 23:03:38 +0600

    solved the 4th problem

--- script.js
+++ script.js
@@ -53,3 +53,20 @@ function strCombo(str) {
 }
 
 console.log(strCombo('dog'));
+
+/* 4. Sort String Alphabetically
+
+Write a JavaScript function that returns a string that has letters in alphabetical order.
+
+Example string : 'webmaster'
+
+Expected Output : 'abeemrstw'
+
+Assume punctuation and numbers symbols are not included in the passed string. */
+
+function strSort(str) {
+  return str.replace(/\s/g, '').split('').sort().join(' ');
+}
+
+console.log(strSort('my name is rizwan siddiqui'));
+  
